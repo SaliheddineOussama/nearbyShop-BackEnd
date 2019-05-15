1b5c3afb0f08b7ed66abefefb53c759be00e2b86
Saliheddine Oussama <oussam.salih@outlook.fr>
2019-05-15 14:15:30 +0000

    Initializing the spring boot project which define our microservice

---
+++ src/main/java/com/shops/NearbyShopsBackEndPartApplication.java
@@ -0,0 +1,13 @@
+package com.shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NearbyShopsBackEndPartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NearbyShopsBackEndPartApplication.class, args);
+	}
+
+}
